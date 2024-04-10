bab49d2ab85597b7f144abfa68efde75b08fe2b7
Bhuvibhuvanesh <bhuvaneshwaran23081999@gmail.com>
2024-04-10 19:12:55 +0530

    product service updated

--- src/main/java/com/expenses/ExpenseTracker/product/ProductService.java
+++ src/main/java/com/expenses/ExpenseTracker/product/ProductService.java
@@ -5,45 +5,53 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.CanteenManagementSystem.model.CanteenManager;
+
 @Service
 public class ProductService {
 
 	@Autowired
 	ProdductRepository productrepository;
+
 	public Product addProduct(Product product) {
 		productrepository.save(product);
 		return product;
-		
-		
+
 	}
 
 	public List<Product> getProducts() {
-		List<Product> pro=(List<Product>) productrepository.findAll();
+		List<Product> pro = (List<Product>) productrepository.findAll();
 		return pro;
-		}
+	}
 
 	public Product getProductById(int id) {
-		Optional<Product> product=productrepository.findById(id);
-		if(product.isPresent()) {
-			Product pro=product.get();
+		Optional<Product> product = productrepository.findById(id);
+		if (product.isPresent()) {
+			Product pro = product.get();
 			return pro;
-		}else {
+		} else {
 			throw new ProductNotFoundException("Given data is  not found");
 		}
-		
+
 	}
 
 	public Product updateProduct(int id, Product product) {
-		Optional<Product> prod=productrepository.findById(product.getId());
-		if(prod.isPresent()) {
-			Product pro=prod.get();
-			pro.setName(pro.getName());
+		Optional<Product> prod = productrepository.findById(product.getId());
+		if (prod.isPresent()) {
+			Product pro = prod.get();
+			if (product.getName() != null) {
+				pro.setName(product.getName());
+			}
+			if(product.getCategory() !=null) {
+				pro.setCategory(product.getCategory());
+			}
 		}
 		return null;
 	}
-
+	
 	public void deleteProduct(int id) {
-		
+
 	}
 
 }
